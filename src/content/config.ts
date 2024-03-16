7549cccfcf9965064d86ec91efebe2a534a5b71b
Green Gem <chris_waitt@me.com>
2024-03-16 14:32:55 +0000

    Add config file for project collections

---
+++ src/content/config.ts
@@ -0,0 +1,21 @@
+import { z, defineCollection } from 'astro:content';
+
+const projectCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+      title: z.string(),
+      description: z.string(),
+      image: z.object({
+        url: z.string(),
+        alt: z.string()
+      }),
+      gitHubUrl: z.string(),
+      previewUrl: z.string(),
+      tags: z.array(z.string()),
+      techStack: z.array(z.string()),
+    }),
+  });
+
+export const collections = {
+  'project': projectCollection,
+};
