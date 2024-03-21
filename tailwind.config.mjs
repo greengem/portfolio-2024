9bd0b2e3162c8480e9926991f5399b6a9df41c25
Green Gem <chris_waitt@me.com>
2024-03-21 17:34:23 +0000

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./public/**/*.{html,js,css,svg}'
+	],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require("@catppuccin/tailwindcss")({
+          prefix: "ctp",
+        //   defaultFlavour: "mocha",
+        }),
+      ],
 }
